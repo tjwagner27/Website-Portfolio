f96d63af3b5068c808505878365c0fc2311827eb
tjwagner27 <89555984+tjwagner27@users.noreply.github.com>
2022-11-03 13:05:08 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -14,7 +14,7 @@ $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
 $header .= "Reply-To: $email";
-header("Access-Control-Allow-Origin: *");
+$header("Access-Control-Allow-Origin: *");
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
