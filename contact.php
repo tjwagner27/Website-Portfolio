71dbf1287f430d0f507364e853047540a6078d9e
tjwagner27 <89555984+tjwagner27@users.noreply.github.com>
2022-11-03 13:02:18 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -13,7 +13,8 @@ $to = "t_wagner_27@hotmail.com"; // Change this email to your //
 $subject = "$m_subject:  $name";
 $body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\n\nEmail: $email\n\nSubject: $m_subject\n\nMessage: $message";
 $header = "From: $email";
-$header .= "Reply-To: $email";	
+$header .= "Reply-To: $email";
+header("Access-Control-Allow-Origin: *");
 
 if(!mail($to, $subject, $body, $header))
   http_response_code(500);
